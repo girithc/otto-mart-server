87b977957fcbae7fd1df898a6baf9d1ede90577a
girithc <girithmchoudhary@gmail.com>
2023-07-29 10:00:09 +0530

    testing go http  - next pgdb

--- http-servers.go
+++ http-servers.go
@@ -2,41 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-    fmt.Fprintf(w, "hello World\n")
-}
-
-func categories(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "List of Product Categories")
-	fmt.Fprintf(res, "name: "+req.FormValue("name"))
-}
-
-func queryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
-    io.WriteString(res, "name: "+req.FormValue("name"))
-    io.WriteString(res, "\nphone: "+req.FormValue("phone"))
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+func getCategoriesList(res http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(res, "Categories List \n")
 }
 
 func main() {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		values := r.URL.Query()
 
-    http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
-	http.HandleFunc("/categories", categories)
-	http.HandleFunc("/example", func(res http.ResponseWriter, req *http.Request) {
-        queryParamDisplayHandler(res, req)
-    })
-    http.ListenAndServe(":8080", nil)
+        for k, v := range values {
+            fmt.Println(k, " => ", v)
+        }
+	})
+
+    http.HandleFunc("/categories", getCategoriesList)
+	http.ListenAndServe(":3000", nil)
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Run Go File
+
+go run filename
+
+# Test Http Connection
+
+curl localhost:8090/hello
