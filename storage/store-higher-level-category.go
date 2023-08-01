dd7a353823f28e5733f1facb9514ebce38880e1f
girithc <girithmchoudhary@gmail.com>
2023-08-01 23:58:54 +0530

    database mapping

--- storage/store-higher-level-category.go
+++ storage/store-higher-level-category.go
@@ -1,2 +1,24 @@
 package storage
 
+import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func (s *PostgresStore) CreateHigherLevelCategoryTable() error {
+	fmt.Println("Entered CreateHigherLevelCategoryTable")
+
+	query := `create table if not exists higher_level_category (
+		higher_level_category_id SERIAL PRIMARY KEY,
+    	higher_level_category_name VARCHAR(100) NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		created_by INT
+	)`
+
+	_, err := s.db.Exec(query)
+
+	fmt.Println("Exiting CreateHigherLevelCategoryTable")
+
+	return err
+}
