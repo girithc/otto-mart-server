96a9189174eed90f32b25f900448cf7a173fd539
girithc <girithmchoudhary@gmail.com>
2023-09-05 18:56:58 +0530

    wokrer-pool

--- worker/worker-pool.go
+++ worker/worker-pool.go
@@ -2,7 +2,9 @@ package worker
 
 import (
 	"fmt"
+	"net/http"
 	"sync"
+	"time"
 )
 
 type WorkerPool struct {
@@ -12,6 +14,12 @@ type WorkerPool struct {
     mu          sync.Mutex
     workerID    int // Add a unique worker ID counter
 }
+
+type Response struct {
+    *http.Response
+    err error
+}
+
 
 func NewWorkerPool(workerCount int) *WorkerPool {
     return &WorkerPool{
@@ -45,4 +53,75 @@ func (wp *WorkerPool) StartWorker(task func() error, resultCallback func(error))
 
 func (wp *WorkerPool) Wait() {
     wp.wg.Wait()
+}
+
+// Dispatcher for making HTTP GET requests
+func (wp *WorkerPool) DispatchGETRequests(reqs int, maxConcurrent int, url string) (int64, int64, time.Duration) {
+    reqChan := make(chan *http.Request)
+    respChan := make(chan Response)
+    start := time.Now()
+
+    // Start the dispatcher and worker pool
+    go wp.dispatcher(reqChan, reqs)
+    go wp.workerPool(reqChan, respChan, maxConcurrent)
+
+    // Consume responses
+    conns, size := wp.consumer(respChan)
+
+    took := time.Since(start)
+    return conns, size, took
+}
+
+// Dispatcher
+func (wp *WorkerPool) dispatcher(reqChan chan *http.Request, reqs int) {
+    defer close(reqChan)
+    for i := 0; i < reqs; i++ {
+        req, err := http.NewRequest("GET", "http://localhost/", nil)
+        if err != nil {
+            fmt.Println(err)
+        }
+        reqChan <- req
+    }
+}
+
+// Worker Pool
+func (wp *WorkerPool) workerPool(reqChan chan *http.Request, respChan chan Response, maxConcurrent int) {
+    t := &http.Transport{}
+    for i := 0; i < maxConcurrent; i++ {
+        go wp.worker(t, reqChan, respChan)
+    }
+}
+
+// Worker
+func (wp *WorkerPool) worker(t *http.Transport, reqChan chan *http.Request, respChan chan Response) {
+    for req := range reqChan {
+        resp, err := t.RoundTrip(req)
+        r := Response{resp, err}
+        respChan <- r
+    }
+}
+
+// Consumer
+func (wp *WorkerPool) consumer(respChan chan Response) (int64, int64) {
+    var (
+        conns int64
+        size  int64
+    )
+    for conns < int64(wp.workerID) { // Use workerID as reqs
+        select {
+        case r, ok := <-respChan:
+            if ok {
+                if r.err != nil {
+                    fmt.Println(r.err)
+                } else {
+                    size += r.ContentLength
+                    if err := r.Body.Close(); err != nil {
+                        fmt.Println(err)
+                    }
+                }
+                conns++
+            }
+        }
+    }
+    return conns, size
 }
