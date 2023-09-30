f1ca7773f3766a0b832e0145f309ea14c1ae0e40
girithc <girithmchoudhary@gmail.com>
2023-09-30 13:20:27 +0530

    cleanup - google server - deprecated

--- api/server.go
+++ api/server.go
@@ -1,71 +1,36 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+
 	"pronto-go/store"
 	"pronto-go/worker"
-
-	iam "google.golang.org/api/iam/v1"
-	"google.golang.org/api/option"
 )
 
 type Server struct {
 	listen_address string
-	store *store.PostgresStore
-	workerPool *worker.WorkerPool
+	store          *store.PostgresStore
+	workerPool     *worker.WorkerPool
 }
 
 func NewServer(listen_address string, store *store.PostgresStore, workerPool *worker.WorkerPool) *Server {
 	return &Server{
 		listen_address: listen_address,
-		store: store,
-		workerPool: workerPool,
+		store:          store,
+		workerPool:     workerPool,
 	}
 }
 
-type GoogleServer struct {
-	bucket string
-	service_account string
-	service_account_id string
-	iam_service *iam.Service
-}
-
-func NewGoogleServer(bucket string, service_account string) *GoogleServer {
-
-	fmt.Println("Creating Google Server")
-
-	ctx := context.Background()
-	
-	iamService, err := iam.NewService(ctx, option.WithCredentialsFile("./named-totality-296407-5646d400bb37.json"))
-    fmt.Printf("iamService: %v\n", iamService)
-	if err != nil {
-        log.Fatal("iam.Server Error", err)
-    }
-
-	serviceAccountID := "service-account@named-totality-296407.iam.gserviceaccount.com"
-	
-	return &GoogleServer{
-		bucket: bucket,
-		service_account: service_account,
-		service_account_id: serviceAccountID,
-		iam_service: iamService,
-	}
-}
-
-func (s *Server) Run(/*gs *GoogleServer*/) {
-
-
-	//http.HandleFunc("/gcloud/sign", gs.handleGoogleSignManager)
+func (s *Server) Run( /*gs *GoogleServer*/ ) {
+	// http.HandleFunc("/gcloud/sign", gs.handleGoogleSignManager)
 
 	http.HandleFunc("/store/available", makeHTTPHandleFunc(s.handleStoreClient))
 	http.HandleFunc("/store", makeHTTPHandleFunc(s.handleStoreManager))
 
 	http.HandleFunc("/higher-level-category", makeHTTPHandleFunc(s.handleHigherLevelCategory))
-	
+
 	http.HandleFunc("/category-higher-level-mapping", makeHTTPHandleFunc(s.handleCategoryHigherLevelMapping))
 
 	http.HandleFunc("/category", makeHTTPHandleFunc(s.handleCategory))
@@ -84,12 +49,7 @@ func (s *Server) Run(/*gs *GoogleServer*/) {
 
 	fmt.Println("Listening PORT", s.listen_address)
 
-	
 	http.ListenAndServe(s.listen_address, nil)
-
-	//go func() {
-	//	_ = http.ListenAndServe(s.listen_address, nil)
-	//} ()
 
 	s.workerPool.Wait()
 }
@@ -113,8 +73,4 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(v)
-} 
-
-
-
-
+}
--- main.go
+++ main.go
@@ -3,60 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+
 	"pronto-go/api"
 	"pronto-go/store"
 	"pronto-go/worker"
-
-	iam "google.golang.org/api/iam/v1"
 )
-
-var (
-    // iamService is a client for calling the signBlob API.
-    iamService *iam.Service
-
-    // serviceAccountName represents Service Account Name.
-    // See more details: https://cloud.google.com/iam/docs/service-accounts
-    serviceAccountName string
-
-    // serviceAccountID follows the below format.
-    // "projects/%s/serviceAccounts/%s"
-    serviceAccountID string
-
-    // uploadableBucket is the destination bucket.
-    // All users will upload files directly to this bucket by using generated Signed URL.
-    uploadableBucket string
-)
-
-
 
 func main() {
-    
-    fmt.Printf("Starting Pronto-DB\n\n")
-    
+	fmt.Printf("Starting Pronto-DB\n\n")
 
-    workerPool := worker.NewWorkerPool(10) 
+	workerPool := worker.NewWorkerPool(10)
 
-    store, err := store.NewPostgresStore()
-
+	store, err := store.NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
-	} 
-    if err := store.Init(); err != nil {
+	}
+	if err := store.Init(); err != nil {
 		log.Fatal(err)
 	} else {
-        fmt.Println("Store.Init() is successful.")
-    }
-    server := api.NewServer(":3000", store, workerPool)
-    
-    //google_server := api.NewGoogleServer("pronto-bucket", "service-account")
-    //fmt.Println("Created Google Server")
-    //server.Run(google_server)
+		fmt.Println("Store.Init() is successful.")
+	}
+	server := api.NewServer(":3000", store, workerPool)
 
-    server.Run()
+	// google_server := api.NewGoogleServer("pronto-bucket", "service-account")
+	// fmt.Println("Created Google Server")
+	// server.Run(google_server)
+
+	server.Run()
 }
- 
+
 func CheckError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
